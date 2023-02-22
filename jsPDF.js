8e17e840ea2ea8164034893e75a0ea86fa2fd24f
RahmanBrussolo <rahman13@gmail.com>
2023-02-21 23:54:48 -0300

    novo codigo js

--- jsPDF.js
+++ jsPDF.js
@@ -8,15 +8,19 @@ function imprimir() {
   // Cria a tabela com o conteúdo do elemento HTML
   const table = pdf.autoTableHtmlToJson(element);
 
-  // Configura a altura da página de acordo com a altura do conteúdo da tabela
-  const pageSize = pdf.internal.pageSize;
-  const pageHeight = pageSize.height - pdf.autoTable.previous.finalY;
-  pdf.autoTable.previous.finalY = 0;
-  pdf.autoTableEndPosY(pageHeight);
-
   // Adiciona a tabela ao PDF
   pdf.autoTable(table);
 
+  // Configura a altura da página de acordo com a altura do conteúdo da tabela
+  const contentHeight = pdf.autoTable.previous.finalY;
+  const pageSize = pdf.internal.pageSize;
+  const pageHeight = pageSize.height - pdf.autoTable.previous.finalY;
+  const pageCount = Math.ceil(contentHeight / pageHeight);
+  for (let i = 1; i < pageCount; i++) {
+    pdf.addPage();
+    pdf.autoTable(table, { startY: pdf.autoTable.previous.finalY });
+  }
+
   // Imprime o PDF usando um software de impressão de PDFs que suporte impressoras térmicas
-  pdf.output('dataurl');
+  pdf.output('dataurlnewwindow');
 }
