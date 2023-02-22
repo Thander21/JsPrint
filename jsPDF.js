f935766e86652851ac20c20365918aa27c47e7f2
RahmanBrussolo <rahman13@gmail.com>
2023-02-22 00:23:02 -0300

    tese22

--- jsPDF.js
+++ jsPDF.js
@@ -3,18 +3,10 @@ function imprimir() {
   const element = document.getElementById('elementPrint');
 
   // Cria um novo objeto jsPDF com orientação paisagem e medidas de papel para uma bobina de 80mm
-  const pdf = new jsPDF('landscape', 'mm', [80, 297]); // altura de 297mm = A4
+  const pdf = new jsPDF('portrait', 'mm', [80, 100]); // altura de 297mm = A4
 
   // Adiciona o conteúdo do elemento HTML ao PDF
   pdf.fromHTML(
-    element,
-    5, // posição horizontal de início do conteúdo (em mm)
-    0, // posição vertical de início do conteúdo (em mm)
-    {
-      // opções de formatação
-      width: 70, // largura do conteúdo (em mm)
-      height: 287, // limite de altura do conteúdo (em mm)
-    },
     function() {
       // Callback executado após a impressão do PDF
       // Imprime o PDF usando um software de impressão de PDFs que suporte impressoras térmicas
