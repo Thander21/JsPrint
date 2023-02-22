973e7d724759ae3af151893ace073eb509c894e1
RahmanBrussolo <rahman13@gmail.com>
2023-02-22 00:02:40 -0300

    slplittext

--- jsPDF.js
+++ jsPDF.js
@@ -1,52 +1,19 @@
 function imprimir() {
-  // Obtém o elemento HTML desejado pelo ID
   const element = document.getElementById('elementPrint');
+  const elementHeight = element.scrollHeight;
 
-  // Cria um novo objeto jsPDF com orientação retrato e medidas de papel para uma bobina de 80mm
-  const pdf = new jsPDF('portrait', 'mm', [80, 0]);
-
-  // Cria a tabela com o conteúdo do elemento HTML
-  const table = pdf.autoTableHtmlToJson(element);
-
-  // Configura a altura da página de acordo com a altura do conteúdo da tabela
-  const pageSize = pdf.internal.pageSize;
-  const pageHeight = pageSize.height - pdf.autoTable.previous.finalY;
-  pdf.autoTable.previous.finalY = 0;
-  pdf.autoTableEndPosY(pageHeight);
-
-  // Adiciona a tabela ao PDF
-  pdf.autoTable(table);
-
-  // Abre a janela de impressão
-  window.print();
-}
-
-/*
-
-function imprimir() {
-  // Obtém o elemento HTML desejado pelo ID
-  const element = document.getElementById('elementPrint');
-
-  // Cria um novo objeto jsPDF com orientação retrato e medidas de papel para uma bobina de 80mm
-  const pdf = new jsPDF('portrait', 'mm', [80, 0]);
-
-  // Cria a tabela com o conteúdo do elemento HTML
-  const table = pdf.autoTableHtmlToJson(element);
-
-  // Adiciona a tabela ao PDF
-  pdf.autoTable(table);
-
-  // Configura a altura da página de acordo com a altura do conteúdo da tabela
-  const contentHeight = pdf.autoTable.previous.finalY;
-  const pageSize = pdf.internal.pageSize;
-  const pageHeight = pageSize.height - pdf.autoTable.previous.finalY;
-  const pageCount = Math.ceil(contentHeight / pageHeight);
-  for (let i = 1; i < pageCount; i++) {
-    pdf.addPage();
-    pdf.autoTable(table, { startY: pdf.autoTable.previous.finalY });
-  }
-
-  // Imprime o PDF usando um software de impressão de PDFs que suporte impressoras térmicas
+  const pdf = new jsPDF('portrait', 'mm', 'thermal', false);
+  const margins = {
+    top: 10,
+    bottom: 10,
+    left: 10,
+    right: 10,
+    width: 80
+  };
+  
+  const contentWidth = margins.width - margins.left - margins.right;
+  const pdfContent = pdf.splitTextToSize(element.innerText, contentWidth);
+  pdf.setPage(1);
+  pdf.text(margins.left, margins.top, pdfContent);
   pdf.output('dataurlnewwindow');
 }
-*/
