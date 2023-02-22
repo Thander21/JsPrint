82d1fb56d0373e097b71b2dfab1ecdd48fd8f380
RahmanBrussolo <rahman13@gmail.com>
2023-02-21 23:58:54 -0300

    novo print

--- jsPDF.js
+++ jsPDF.js
@@ -8,6 +8,31 @@ function imprimir() {
   // Cria a tabela com o conteúdo do elemento HTML
   const table = pdf.autoTableHtmlToJson(element);
 
+  // Configura a altura da página de acordo com a altura do conteúdo da tabela
+  const pageSize = pdf.internal.pageSize;
+  const pageHeight = pageSize.height - pdf.autoTable.previous.finalY;
+  pdf.autoTable.previous.finalY = 0;
+  pdf.autoTableEndPosY(pageHeight);
+
+  // Adiciona a tabela ao PDF
+  pdf.autoTable(table);
+
+  // Abre a janela de impressão
+  window.print();
+}
+
+/*
+
+function imprimir() {
+  // Obtém o elemento HTML desejado pelo ID
+  const element = document.getElementById('elementPrint');
+
+  // Cria um novo objeto jsPDF com orientação retrato e medidas de papel para uma bobina de 80mm
+  const pdf = new jsPDF('portrait', 'mm', [80, 0]);
+
+  // Cria a tabela com o conteúdo do elemento HTML
+  const table = pdf.autoTableHtmlToJson(element);
+
   // Adiciona a tabela ao PDF
   pdf.autoTable(table);
 
@@ -24,3 +49,4 @@ function imprimir() {
   // Imprime o PDF usando um software de impressão de PDFs que suporte impressoras térmicas
   pdf.output('dataurlnewwindow');
 }
+*/
